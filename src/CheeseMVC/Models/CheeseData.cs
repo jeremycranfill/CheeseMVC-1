3b4261083e785f993f3310ab8eb5cd70040c2ba9
Jeremy <jeremy.cranfill@gmail.com>
2017-03-23 16:47:28 -0500

    coded along

---
+++ src/CheeseMVC/Models/CheeseData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseData
+    {
+
+        static private List<Cheese> cheeses = new List<Cheese>();
+        
+        //getall
+       public static List<Cheese> GetAll()
+        {
+            return cheeses;
+
+        }
+
+        public static void Add(Cheese newCheese)
+        {
+            cheeses.Add(newCheese);
+        }
+
+        // remove
+
+        public static void Remove(int id)
+        {
+            Cheese cheeseToRempve = GetById(id);
+
+
+        }
+
+        public static Cheese GetById(int id)
+        {
+            return cheeses.Single(x => x.CheeseId == id);
+
+        }
+
+
+    }
+}
